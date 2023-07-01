457d03d3cf062afcd4ca9a2baad4cbda6fd9edc0
franciscapr <francisca.paredes.pr@gmail.com>
2023-07-01 19:32:59 +0000

    Modificacion del form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
 require 'faker'
 
+powers = ["Super fuerza", "Vuelo", "Invisibilidad", "Telequinesis", "Regeneración", "Control elemental"]
+
 100.times do |t|
-    Tweet.create(username: Faker::Superhero.name, description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
-  end
-  
+  username = Faker::Superhero.name
+  power = powers[t % powers.length]  # Asigna el poder de forma secuencial, repitiendo la lista si es necesario
+
+  Tweet.create(
+    username: username,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    power: power
+  )
+end
